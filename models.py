d93e756b65849f5bdc6ed7ccd51a5c62a9f8566a
William Pinho <william.comp.ita@gmail.com>
2013-04-19 23:52:20 -0300

    Function fetch created

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 print "MODELS"
+from filereader import FileReader
 
 instrucao_estagio1="addi R10, R0, 100"
+
+fr = FileReader()
+fr.read("./instructions/program.txt")
+print "Arquivo program.txt carregado"
+
+def fetch(i, filereader):
+	instruction = filereader.getInst(i)
+	return instruction
+
+
+
